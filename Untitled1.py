fe6dd844c181d085fbbe400a5e96264b966b0ea9
Dipanwita-23 <144333881+Dipanwita-23@users.noreply.github.com>
2024-05-23 22:51:37 +0530

    Add files via upload

---
+++ Untitled1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import streamlit as st
+from langchain.llms import OpenAI
+
+st.title('🦜🔗 Quickstart App')
+
+openai_api_key = st.sidebar.text_input('sk-proj-b0EOOjlPOp2iMJw8vzfvT3BlbkFJHoClmwjQXreeyB4tqpGN')
+
+def generate_response(input_text):
+  llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+  st.info(llm(input_text))
+
+with st.form('my_form'):
+  text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+  submitted = st.form_submit_button('Submit')
+  if not openai_api_key.startswith('sk-'):
+    st.warning('Please enter your OpenAI API key!', icon='⚠')
+  if submitted and openai_api_key.stahttp://localhost:8888/notebooks/Documents/AlgoAnalytics/Untitled1.ipynb#rtswith('sk-'):
+    generate_response(text)
+
+
+# In[ ]:
+
+
+
+
